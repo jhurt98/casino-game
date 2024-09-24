9377e6c91ab0eea7e6770438afedde97b1b38d22
nuggs <hurtadojaime96@gmail.com>
2024-09-23 23:38:59 -0700

    drag

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,14 +1,16 @@
-//import { useState } from "react";
+import { useState, useRef } from "react";
 function Card() {
-
+    const [pos, setPos] = useState(undefined);
+    const [drag, setDrag] = useState({x: 0, y: 0});
+    const [dragging, setDragging] = useState(false);
+    const test = useRef(null);
     function gpt() {
         return( 
                <div>
 <svg width="400" height="700" xmlns="http://www.w3.org/2000/svg">
     {/**-- Card Template --*/}
   <defs>
-    <rect id="card" width="60" height="90" rx="10" ry="10" fill="white" stroke="black" />
-    <symbol id="heart" viewBox="0 0 32 29.6">
+    <rect id="card" width="60" height="90" rx="10" ry="10" fill="white" stroke="black" /> <symbol id="heart" viewBox="0 0 32 29.6">
       <path d="M23.6,0c-3.2,0-6.1,1.2-8.6,3.2C12.5,1.2,9.6,0,6.4,0C2.9,0,0,2.9,0,6.4c0,7.3,11.2,12.3,16,19.2c4.8-6.9,16-11.9,16-19.2 C32,2.9,29.1,0,25.6,0C24.8,0,24.1,0.2,23.6,0z" fill="red"/>
     </symbol>
     <symbol id="diamond" viewBox="0 0 32 32">
@@ -53,22 +55,40 @@ function Card() {
 </svg>
 </div>)
     }
-    function handleMouseDown(e: React.MouseEvent) {
-        const x = e.pageX;
-        const y = e.pageY;
 
-        console.log(x, y);
-        console.log(e);
+    function handleMouseDown(e: React.MouseEvent) {
+        e.preventDefault();
+        drag.x = e.clientX;
+        drag.y = e.clientY;
+        console.log(e.target.getBoundingClientRect());
+        setDragging(true);
     }
 
     function handleMouseMove(e: React.MouseEvent) {
+        e.preventDefault();
+        console.log(drag);
+        const xDiff = drag.x - e.clientX;
+        const yDiff = drag.y - e.clientY;
+        drag.x = e.clientX;
+        drag.y = e.clientY;
+        console.log(drag, xDiff, yDiff);
+        if (test && test.current) {
+            let newTop = `${test.current.offsetTop - yDiff}px`;
+            let newLeft = `${test.current.offsetLeft - xDiff}px`;
+            let newPos = { top: newTop, left: newLeft };
+            setPos(newPos);
+        }
     }
+    let dynamicStyle = pos === undefined ? {} : { top: pos.top, left: pos.left };
   return (
     <>
-    <div>
-    </div>
-    {gpt()}
-    <div className="playingCard" onMouseDown={handleMouseDown}>
+    {/*gpt()*/}
+    <div className="playingCard"
+         onMouseDown={handleMouseDown} 
+         onMouseMove={dragging ? handleMouseMove : undefined } 
+         onMouseUp={() => setDragging(false)} ref={test}
+         style={dynamicStyle}
+    >
       <div className="content">
         <p>4</p>
         <svg
@@ -79,7 +99,7 @@ function Card() {
           clipRule="evenodd"
         >
           <path
-            d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"
+            d="m12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"
           />
         </svg>
       </div>
@@ -94,7 +114,7 @@ function Card() {
           clipRule="evenodd"
         >
           <path
-            d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"
+            d="m12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"
           />
         </svg>
       </div>
